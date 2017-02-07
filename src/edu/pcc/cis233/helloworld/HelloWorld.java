b459a560a5734dcc5a5ea847d109ac9525d6a084
Mark Curran <mark.curran@pcc.edu>
2017-02-06 22:43:02 -0800

    added main method that prints hello world

--- src/edu/pcc/cis233/helloworld/HelloWorld.java
+++ src/edu/pcc/cis233/helloworld/HelloWorld.java
@@ -3,5 +3,10 @@ package edu.pcc.cis233.helloworld;
 /**
  * Created by Mark on 2/6/2017.
  */
-public class HelloWorld {
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!");
+    }
 }
