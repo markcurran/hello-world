c4dfeb33214dd67eab3d0334ea2a3c46500ba836
Mark Curran <mark.curran@pcc.edu>
2017-02-06 22:49:25 -0800

    print date

--- src/edu/pcc/cis233/helloworld/HelloWorld.java
+++ src/edu/pcc/cis233/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis233.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Mark on 2/6/2017.
  */
@@ -8,5 +10,6 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!!");
+        System.out.println(new Date());
     }
 }
